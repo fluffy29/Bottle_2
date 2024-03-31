bd025833350e6d44992d9fc01d084b107fc36956
fluffy29 <hmajzoub12@gmail.com>
2024-03-31 17:28:46 +0200

    first commit

---
+++ bottle_2.py
@@ -0,0 +1,5 @@
+for i in range(100 , 0, -1):
+  print(str(i) + ' bottles of the beer on the wall')
+  print(str(i) + ' bottles of beer')
+  print(' Take one down, pass it around')
+  print(str(i-1) + ' bottles of beer on the wall')
